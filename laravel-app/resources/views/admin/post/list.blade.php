2e050ee11cc12b42b215a87a2dae9d3e3d8b5861
test <40716131+YuliiaHruzd@users.noreply.github.com>
2025-03-08 13:59:03 +0100

    add laravel admin post controller

---
+++ laravel-app/resources/views/admin/post/list.blade.php
@@ -0,0 +1,118 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Data Posts</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <div id="dataTable_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <a href="post/create" class="text-primary"> Create Post</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <table class="table table-bordered dataTable" id="dataTable" width="100%"
+                                       cellspacing="0" role="grid" aria-describedby="dataTable_info"
+                                       style="width: 100%;">
+                                    <thead>
+                                    <tr role="row">
+                                        <th class="sorting sorting_asc" tabindex="0" aria-controls="dataTable"
+                                            rowspan="1" colspan="1" aria-sort="ascending"
+                                            aria-label="Name: activate to sort column descending" style="width: 228px;">
+                                            ID
+                                        </th>
+                                        <th class="sorting sorting_asc" tabindex="0" aria-controls="dataTable"
+                                            rowspan="1" colspan="1" aria-sort="ascending"
+                                            aria-label="Name: activate to sort column descending" style="width: 228px;">
+                                            Name
+                                        </th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1"
+                                            colspan="1" aria-label="Position: activate to sort column ascending"
+                                            style="width: 343px;">Text
+                                        </th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1"
+                                            colspan="1" aria-label="Office: activate to sort column ascending"
+                                            style="width: 166px;">Author Id
+                                        </th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1"
+                                            colspan="1" aria-label="Age: activate to sort column ascending"
+                                            style="width: 81px;">Date created
+                                        </th>
+                                        <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1"
+                                            colspan="1" aria-label="Start date: activate to sort column ascending"
+                                            style="width: 158px;">Actions
+                                        </th>
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+
+                                    @foreach ($posts as $post)
+                                        <tr class="odd">
+                                            <td class="sorting_1">{{$post->id}}</td>
+                                            <td >{{$post->name}}</td>
+                                            <td>{{$post->text}}</td>
+                                            <td>{{$post->author_id}}</td>
+                                            <td>{{$post->created_at}}</td>
+                                            <td>
+                                                <a href="post/update/{{$post->id}}">update</a>
+                                                <br><a href="post/delete/{{$post->id}}">delete</a>
+                                            </td>
+                                    @endforeach
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 col-md-5">
+                                <div class="dataTables_info" id="dataTable_info" role="status" aria-live="polite">
+                                    Showing 1 to 10 of 57 entries
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-7">
+                                <div class="dataTables_paginate paging_simple_numbers" id="dataTable_paginate">
+                                    <ul class="pagination">
+                                        <li class="paginate_button page-item previous disabled" id="dataTable_previous">
+                                            <a href="#" aria-controls="dataTable" data-dt-idx="0" tabindex="0"
+                                               class="page-link">Previous</a></li>
+                                        <li class="paginate_button page-item active"><a href="#"
+                                                                                        aria-controls="dataTable"
+                                                                                        data-dt-idx="1" tabindex="0"
+                                                                                        class="page-link">1</a></li>
+                                        <li class="paginate_button page-item "><a href="#" aria-controls="dataTable"
+                                                                                  data-dt-idx="2" tabindex="0"
+                                                                                  class="page-link">2</a></li>
+                                        <li class="paginate_button page-item "><a href="#" aria-controls="dataTable"
+                                                                                  data-dt-idx="3" tabindex="0"
+                                                                                  class="page-link">3</a></li>
+                                        <li class="paginate_button page-item "><a href="#" aria-controls="dataTable"
+                                                                                  data-dt-idx="4" tabindex="0"
+                                                                                  class="page-link">4</a></li>
+                                        <li class="paginate_button page-item "><a href="#" aria-controls="dataTable"
+                                                                                  data-dt-idx="5" tabindex="0"
+                                                                                  class="page-link">5</a></li>
+                                        <li class="paginate_button page-item "><a href="#" aria-controls="dataTable"
+                                                                                  data-dt-idx="6" tabindex="0"
+                                                                                  class="page-link">6</a></li>
+                                        <li class="paginate_button page-item next" id="dataTable_next"><a href="#"
+                                                                                                          aria-controls="dataTable"
+                                                                                                          data-dt-idx="7"
+                                                                                                          tabindex="0"
+                                                                                                          class="page-link">Next</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
