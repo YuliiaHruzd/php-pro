e6da80f6daad97b7dffaa6906a914ee4f30a988d
test <40716131+yuliiahruzd@users.noreply.github.com>
2025-03-22 12:00:18 +0100

    add user id

--- laravel-app/database/migrations/0001_01_01_000005_create_cart_table.php
+++ laravel-app/database/migrations/0001_01_01_000005_create_cart_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('cart', function (Blueprint $table) {
             $table->id();
             $table->integer('post_id');
+            $table->integer('user_id')->default(1);
             $table->integer('count')->default(1);
             $table->timestamps();
         });
