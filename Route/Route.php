2000004cd63bf70e236d09e9c6565ca3658d854a
test <40716131+yuliiahruzd@users.noreply.github.com>
2025-03-30 19:11:15 +0200

    add notes controller

--- Route/Route.php
+++ Route/Route.php
@@ -14,6 +14,11 @@ class Route
             'folder/update/from/:d/to/:d' => ['className' => 'Folder', 'action' => 'update', 'method' => 'GET'],
             'folder/show/:d' => ['className' => 'Folder', 'action' => 'show', 'method' => 'GET'],
             'folder/delete/:d' => ['className' => 'Folder', 'action' => 'delete', 'method' => 'GET'],
+            'note/index' => ['className' => 'Note', 'action' => 'index', 'method' => 'GET'],
+            'note/create/:d' => ['className' => 'Note', 'action' => 'create', 'method' => 'GET'],
+            'note/update/from/:d/to/:d' => ['className' => 'Note', 'action' => 'update', 'method' => 'GET'],
+            'note/show/:d' => ['className' => 'Note', 'action' => 'show', 'method' => 'GET'],
+            'note/delete/:d' => ['className' => 'Note', 'action' => 'delete', 'method' => 'GET'],
             '' => ['className' => 'Main', 'action' => 'index', 'method' => 'GET'],
             'default' => ['className' => 'Error', 'action' => 'index'],
         ];
